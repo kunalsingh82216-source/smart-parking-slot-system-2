5c22894eb57ff14e2780d906fc41adaaa64ddd98
kunal singh <kunalsingh1356729@gmail.com>
2026-03-15 19:24:43 +0530

    Add QR scan component

---
+++ src/components/QRScanComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const QRScanComponent = () => {
+    return (
+        <div>
+            <h1>QR Code Scan Component</h1>
+            {/* Your QR scanning logic goes here */}
+        </div>
+    );
+};
+
+export default QRScanComponent;
